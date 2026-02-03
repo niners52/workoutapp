8711d611a3c51ef424ef136249a5e629e5be79e6
Tyler <tyler.jensen82@gmail.com>
2026-02-03 15:31:29 -0600

    fix: add missing SwiftUI import to Watch app files

--- plugins/watch-app/swift/WatchViewModel.swift
+++ plugins/watch-app/swift/WatchViewModel.swift
@@ -1,6 +1,7 @@
 import Foundation
 import WatchConnectivity
 import Combine
+import SwiftUI
 
 class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
     @Published var isWorkoutActive = false
